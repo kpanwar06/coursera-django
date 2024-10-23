ed368029990b402137fe4a0184ddff06d187e140
kpanwar06 <kritika20.panwar@gmail.com>
2024-10-23 17:43:27 +0530

    update

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class cake_order(models.Model):
+    flavours=((1,"Chocolate"),(2,"Strawberry"),(3,"Vanilla"))
     f_name=models.CharField(max_length=100)
     order_date=models.DateField()
-    cake_flavour=models.CharField(max_length=100)
+    cake_flavour=models.IntegerField(max_length=10,choices=flavours)
