1df479c3d55ede5c9082738c638d03c70c431f19
kpanwar06 <kritika20.panwar@gmail.com>
2024-10-27 10:25:34 +0530

    updated

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-from myapp.models import cake_order
+from myapp.models import cake_order,customer_details
 admin.site.register(cake_order)
+admin.site.register(customer_details)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,4 +5,14 @@ class cake_order(models.Model):
     flavours=((1,"Chocolate"),(2,"Strawberry"),(3,"Vanilla"))
     f_name=models.CharField(max_length=100)
     order_date=models.DateField()
-    cake_flavour=models.IntegerField(max_length=10,choices=flavours)
+    cake_flavour=models.IntegerField(choices=flavours)
+
+    def __str__(self):
+        return self.f_name
+
+class customer_details(models.Model):
+    f_name=models.CharField(max_length=100)
+    phone_no=models.IntegerField()
+    customer_id=models.ForeignKey(cake_order,on_delete=models.PROTECT,default=None)
+    def __str__(self):
+        return self.f_name
